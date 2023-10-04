b42c8d9f9503b3ada8e42c238f6b66c4f93e35a2
Doe-P <phoukhando@gmail.com>
2023-10-04 10:53:19 +0700

    update information feature

---
+++ lib/features/information/domain/repository/information_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:galaxy_18_lottery_app/shared/domain/models/infomation/information_model.dart';
+import 'package:galaxy_18_lottery_app/shared/domain/models/paginated_response.dart';
+import 'package:galaxy_18_lottery_app/shared/exceptions/http_exception.dart';
+
+abstract class InformationRepository {
+  Future<Either<AppException, PaginatedResponse<Information>>> fetchInformation();
+}
